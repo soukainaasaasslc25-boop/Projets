dadc9cd8a4ae78ba3ca128ad0eb6f0bd96261789
soukaina <soukaina.asaas.slc25@gmail.com>
2025-09-22 09:57:15 +0100

    réalisation dun jeu éducatif

---
+++ Realisation/README.md
@@ -0,0 +1,11 @@
+# Jeu des Compétences - Solicode
+
+Ce projet est un petit **jeu éducatif en JavaScript** qui permet de mémoriser les 8 compétences principales de la formation Solicode.
+
+##  Fonctionnement du jeu
+
+ L'utilisateur entre son **nom** au début de la partie.
+Le programme pose une série de questions :
+   - Il affiche le **nom d'une compétence**.
+   - L'utilisateur doit taper le **code ** (C1 à C8).
+ À la fin du jeu, un **score final** est affiché ainsi qu'un message de motivation.
---
+++ Realisation/javascript.js
@@ -0,0 +1,53 @@
+
+let   personalP = prompt("Veuillez entrer votre nom pour commencer le jeu:");
+
+
+const codes = ["C1", "C2", "C3", "C4", "C5", "C6", "C7", "C8"];
+
+const noms = [
+  "demarche-pedagogique-solicode",
+  "HTML/CSS",
+  "JS",
+  "Bases de données",
+  "Node.js",
+  "React",
+  "Gestion de projet",
+  "FrameworkJS"
+];  
+ let score=0;
+
+function jeu() {
+   
+    
+
+for (let i= 0; i< noms.length; i++) {
+    console.log("question" +(i+1))
+     
+let repence= prompt("Quel est le code de la compétence: " +noms[i]);
+if (repence !== "" && repence.toUpperCase() === codes[i]) {
+
+    console.log(" ✅ Correct! La réponse est bien " +codes[i])
+    score++;
+   
+    
+}else(
+    
+    console.log(" ❌  Incorrect. La bonne réponse pour  "+noms[i]+" est "+codes[i])
+)
+
+    
+}
+
+  
+    console.log("Score total  " +score + " /8")
+if ( score <= noms.length / 2) {
+        console.log( `Merci d'avoir joué, ${personalP}! Vous pouvez réessayer pour améliorer votre score.`)   
+}
+else if (score === noms.length) {
+  console.log(` 🌟 Bravo ${personalP}! Excellent travail!`)}
+  else if (score >= noms.length / 2) {
+    console.log("👍 Bien joué ! Continue de t'entraîner.");
+  }
+}
+       
+jeu();
