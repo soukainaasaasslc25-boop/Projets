c09981adb881d32fc376167f2b8b5d0151de11f8
soukaina <soukaina.asaas.slc25@gmail.com>
2025-11-12 08:11:01 +0000

    uppdate

---
+++ bibliotheque/ajouter.js
@@ -0,0 +1,49 @@
+
+  let form =document.getElementById("form");
+form.style.border = "2px solid #d88837ff";
+form.style.padding = "20px";
+form.style.borderRadius = "10px";
+form.style.width = "300px";
+form.style.backgroundColor = "#f9f9f9";
+form.style.display = "flex";
+form.style.flexDirection = "column";
+form.style.gap = "10px";
+
+    document.getElementById("form").onsubmit = function(e) {
+  e.preventDefault();
+
+  const imageInput = document.getElementById("image");
+  const fichier = imageInput.files[0]; // récupère le fichier choisi
+
+  const lecteur = new FileReader();
+
+  lecteur.onload = function(event) {
+   
+    const livre = {
+      code: parseInt(document.getElementById("code").value),
+      titre: document.getElementById("Titre").value,
+      auteur: document.getElementById("Auteur").value,
+      annee: parseInt(document.getElementById("Annee").value),
+      prixDh: parseFloat(document.getElementById("Prix").value),
+      image: event.target.result || "", 
+      disponible: document.getElementById("disponible").checked
+    };
+
+    if (window.opener && !window.opener.closed) {
+      
+      window.opener.ajouterLivre(livre); // envoie le livre au catalogue
+      alert("Livre ajouté !");
+      window.close();
+    } else {
+      alert("La page catalogue n'est pas ouverte !");
+    }
+  };
+
+  // lire le fichier s’il existe, sinon passer une chaîne vide
+  if (fichier) {
+    lecteur.readAsDataURL(fichier);
+  } else {
+    // déclenche l’onload avec une image vide
+    lecteur.onload({ target: { result: "" } });
+  }
+};
